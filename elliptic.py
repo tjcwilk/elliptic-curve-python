9d1268099b8325096a5899d807efd737175efa7a
Toby W <toby@foo>
2020-06-25 09:29:09 +0000

    init repo skeleton

---
+++ elliptic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+class Ellyptic:
+    def __init__(self):
+        print("Ellyptic Object created");
+
+
+    def ping(self):
+        print("Pong!")
+
+
+if __name__ == "__main__":
+
+    ellyptic = Ellyptic()
+    ellyptic.ping()
